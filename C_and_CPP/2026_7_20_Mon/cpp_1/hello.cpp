e3d406253b27a6698a13963b48445f5044351623
fghmnst <wyb211985@outlook.com>
2026-07-22 14:17:46 +0800

    7月22日小更

--- C_and_CPP/2026_7_20_Mon/cpp_1/hello.cpp
+++ C_and_CPP/2026_7_20_Mon/cpp_1/hello.cpp
@@ -4,7 +4,6 @@
 using namespace std;
 int main()
 {
-   
     int a = 0;
     a = 2.3;
     cout << "a = " << a << endl;
---
+++ C_and_CPP/2026_7_22_Wen/cpp_2/negative_index_pointer.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int num_array[5];
+
+    for(int idx = -1; idx < 5; idx++){
+        num_array[idx] = idx*idx;
+    }
+
+    for(int idx = -1; idx < 5; idx++){
+        cout << "num_array[" << idx << "] = " << num_array[idx] << endl;
+    }
+
+    return 0;
+}
